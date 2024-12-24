b4b5b6291ded4ef7b00a749881a89cfa342862b2
Kim Kyeseung <chickenboys@naver.com>
2024-12-24 17:01:56 +0900

    fix: App 모듈에서 하위 모듈 import 하지 않은 문제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { LectureController } from './infrastructure/http/lecture.controller';
 import { AppService } from './app.service';
+import { LectureService } from './application/lecture/lecture.service';
+import { DatabaseModule } from './infrastructure/database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule],
+  controllers: [AppController, LectureController],
+  providers: [AppService, LectureService],
 })
 export class AppModule {}
