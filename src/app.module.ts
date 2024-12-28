2bb8c7e60de3a244cb84133371988acfa219c213
Kim Kyeseung <chickenboys@naver.com>
2024-12-28 14:15:11 +0900

    fix: databaseconfig 미사용 코드

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { LectureController } from './infrastructure/http/lecture.controller';
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LectureController } from './infrastructure/http/lecture.controller';
 import { LectureService } from './application/lecture/lecture.service';
 import { DatabaseModule } from './infrastructure/database/database.module';
-import { typeOrmTestConfig } from './infrastructure/config/typeorm.config';
+import {
+  databaseConfig,
+  typeOrmTestConfig,
+} from './infrastructure/config/typeorm.config';
+
+const typeOrmConfig =
+  process.env.NODE_ENV === 'test' ? typeOrmTestConfig : databaseConfig;
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmTestConfig), DatabaseModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), DatabaseModule],
   controllers: [AppController, LectureController],
   providers: [AppService, LectureService],
 })
